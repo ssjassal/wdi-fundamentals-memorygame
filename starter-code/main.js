a8e6c8e82039691d99f1ecc547e74a08e25978fe
527579 <soniyajassal@C02NN3P2G3QP.local>
2017-03-01 00:06:35 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,11 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = null;
+var cardTwo = null;
+var cardThree = null;
+var cardFour = null;
+
+
+cardOne = "Queen";
+cardTwo = "Queen";
+cardThree = "King";
+cardFour = "King";
