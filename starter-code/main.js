6b9d1e530e268788efebf142cc8bdb8e846c7313
527579 <soniyajassal@C02NN3P2G3QP.local>
2017-03-01 14:53:25 -0500

    attempting createElement

--- starter-code/main.js
+++ starter-code/main.js
@@ -10,12 +10,16 @@ cardTwo = "Queen";
 cardThree = "King";
 cardFour = "King";
 
-if (cardOne == cardTwo) || (cardThree == cardFour) 
-{
-	alert('You have found a match!');
+// if (cardOne == cardTwo) || (cardThree == cardFour) 
+// {
+// 	alert('You have found a match!');
 
-}
-else 
-{
-	alert ('Sorry try again.');
-}
+// }
+// else 
+// {
+// 	alert ('Sorry try again.');
+// }
+
+var getListId = document.getElementById('game-board');
+	getListId.innerHTML = document.createElement ('div');
+	
